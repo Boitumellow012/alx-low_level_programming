ce77b6e2bd7493a78ea400c472fac81481e09179
Boitumellow012 <brakobeloa@gmail.com>
2023-08-25 15:57:09 -0700

    file up

---
+++ 0x18-dynamic_libraries/math.c
@@ -0,0 +1,60 @@
+/**
+ * add - Returns the sum of two integers.
+ * @a: The first integer.
+ * @b: The second integer.
+ *
+ * Return: The sum of a and b.
+ */
+int add(int a, int b)
+{
+    return (a + b);
+}
+
+/**
+ * sub - Returns the result of subtracting b from a.
+ * @a: The first integer.
+ * @b: The second integer.
+ *
+ * Return: The result of the subtraction.
+ */
+int sub(int a, int b)
+{
+    return (a - b);
+}
+
+/**
+ * mod - Returns the remainder when a is divided by b.
+ * @a: The dividend.
+ * @b: The divisor.
+ *
+ * Return: The remainder of the division.
+ */
+int mod(int a, int b)
+{
+    return (a % b);
+}
+
+/**
+ * mul - Returns the product of two integers.
+ * @a: The first integer.
+ * @b: The second integer.
+ *
+ * Return: The product of a and b.
+ */
+int mul(int a, int b)
+{
+    return (a * b);
+}
+
+/**
+ * div - Returns the integer division result of a divided by b.
+ * @a: The dividend.
+ * @b: The divisor.
+ *
+ * Return: The integer division result.
+ */
+int div(int a, int b)
+{
+    return (a / b);
+}
+
